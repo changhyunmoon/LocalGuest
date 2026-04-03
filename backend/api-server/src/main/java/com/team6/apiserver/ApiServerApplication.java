d3a0fb267e7ac437c2b3d6530b7b8b348981d4d9
jjang0617 <130431926+jjang0617@users.noreply.github.com>
2026-04-03 18:05:48 +0900

    Update repository annotations in ApiServerApplication
    Refactor repository annotations for clarity and specificity.

--- backend/api-server/src/main/java/com/team6/apiserver/ApiServerApplication.java
+++ backend/api-server/src/main/java/com/team6/apiserver/ApiServerApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.team6") // 서비스, 컨트롤러 스캔
-@EnableJpaRepositories(basePackages = "com.team6") // 레포지토리 스캔
-@EntityScan(basePackages = "com.team6") // @Entity 위치 지정
-@EnableMongoRepositories(basePackages = "com.team6")
+@ComponentScan(basePackages = "com.team6") // 컨트롤러, 서비스 등 모든 컴포넌트 스캔
+@EntityScan(basePackages = "com.team6")    // 모든 모듈의 JPA 엔티티 스캔
+@EnableJpaRepositories(
+        basePackages = {
+                "com.team6.module.chat.repository.mysql", // 채팅용 JPA 레포지토리
+        }
+)
+@EnableMongoRepositories(
+        basePackages = "com.team6.module.chat.repository.mongodb" // 채팅용 MongoDB 레포지토리
+)
 public class ApiServerApplication {
 
     public static void main(String[] args) {
