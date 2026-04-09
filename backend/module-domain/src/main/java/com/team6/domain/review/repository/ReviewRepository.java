1b17b37b4a9175096c19745a6f2f4c38a62b39a5
Jimin Pyo <dkwlrhdwn20@gmail.com>
2026-04-09 17:47:57 +0900

    Fix/be:SecurityUtil mergeí의존성주RevieReview Repository 의존성 주입 해결

--- backend/module-domain/src/main/java/com/team6/domain/review/repository/ReviewRepository.java
+++ backend/module-domain/src/main/java/com/team6/domain/review/repository/ReviewRepository.java
@@ -2,9 +2,10 @@ package com.team6.domain.review.repository;
 
 import com.team6.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByGuideId(Long guideId);
 }
