ae7eafe6defac4ee574f3351c2aa4140e5d8c6f9
문창현 <jake123456@naver.com>
2026-04-14 14:56:44 +0900

    tmp

--- backend/module-domain/src/main/java/com/team6/domain/auth/config/SecurityConfig.java
+++ backend/module-domain/src/main/java/com/team6/domain/auth/config/SecurityConfig.java
@@ -46,6 +46,9 @@ public class SecurityConfig {
                         // 2. 리뷰 조회는 비로그인 유저도 가능
                         .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll()
 
+                        // Actuator 경로 추가 (배포시 헬스채크용)
+                        .requestMatchers("/actuator/**").permitAll()
+
                         // 3. 그 외 (리뷰 등록, 채팅, 마이페이지 등)는 무조건 로그인 필요
                         .anyRequest().authenticated()
                 );
