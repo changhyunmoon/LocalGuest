804e16750b84b5046bb773e239fcfc0468b314d8
Jimin <168080522+JIMIN-1211@users.noreply.github.com>
2026-04-15 17:37:24 +0900

    redisConfig 오류 IntegratedRedisConfig생성으로 해결 (#207)

--- backend/module-common/src/main/java/com/team6/module/common/global/config/IntegratedRedisConfig.java
+++ backend/module-common/src/main/java/com/team6/module/common/global/config/IntegratedRedisConfig.java
@@ -1,30 +1,47 @@
-package com.team6.module.chat.config;
+package com.team6.module.common.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration("chatRedisConfig")
-public class RedisConfig {
+@Configuration
+public class IntegratedRedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean(name = "memberRedisTemplate")
+    public RedisTemplate<String, String> memberRedisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
 
     @Bean(name = "chatRedisTemplate")
-    public RedisTemplate<String, Object> chatRedisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> chatRedisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-
-        // 1. Key 직렬화: String으로 저장
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
 
-        // 2. Value 직렬화: JSON으로 저장
-        // ObjectMapper 설정을 통해 LocalDatetime 등 자바 8 시간 타입도 처리 가능하게 함
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
         redisTemplate.setValueSerializer(serializer);
