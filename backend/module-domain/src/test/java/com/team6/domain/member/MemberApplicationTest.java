36620f01c4c4cc7c6707c7c5c2647fa03cadd1a9
Jimin Pyo <dkwlrhdwn20@gmail.com>
2026-04-20 12:20:02 +0900

    Feat : guestProfile 분리 및 member스키마 수정

--- backend/module-domain/src/test/java/com/team6/domain/member/MemberApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.team6.domain.member;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-@SpringBootApplication
-@Import(TestPasswordConfig.class)
-public class MemberApplicationTest {
-}
