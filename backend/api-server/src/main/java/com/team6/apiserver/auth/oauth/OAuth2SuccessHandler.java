6e4fa357b0d4eba20aac3dad439581516d923f14
Jimin <168080522+JIMIN-1211@users.noreply.github.com>
2026-04-24 18:50:24 +0900

    Feat/be#500 가이드 소셜로그인 후 경로 하드코딩 제거 (#501)
    * Feat : 가이드, 게스트 프로필 이미지 등록 ë
    
    * Feat/fe : 게스트, 가이드 마이페이지 이미지 등록 반영
    
    * Feat/fe : 리뷰 작성 모달 이미지 업로드 버튼 삭제
    
    * Feat : 게스트, 가이드 마이í이지 프로필 수정 UI통일
    
    * Feat : 프론트 UI/UX 개선
    
    * Feat : 프론트 ãUI개선
    
    * fix : frontend .env 백엔드 경로 임시로 로컬로 변경해둠 테스트용
    
    * Feat : 프론ã트 개선
    
    * Feat : 메인페이지 UI/UX수정
    
    * Feat : 프메인 페이지 UI디자인 수정
    
    * Feat : 가이드 등록 페이지 수정
    
    * Feat : 가이드 소셜로그인 로직 개선 및 수정
    
    * Remove log file
    
    * fix:application.yml
    
    * Feat : OAuth2 경로 하드코딩 제거

--- backend/api-server/src/main/java/com/team6/apiserver/auth/oauth/OAuth2SuccessHandler.java
+++ backend/api-server/src/main/java/com/team6/apiserver/auth/oauth/OAuth2SuccessHandler.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtTokenProvider jwtTokenProvider;
 
-    @Value("${app.oauth2.frontend-redirect-base:http://localhost:5173}")
+    @Value("${app.oauth2.frontend-redirect-base}")
     private String frontendRedirectBase;
 
     @Override
