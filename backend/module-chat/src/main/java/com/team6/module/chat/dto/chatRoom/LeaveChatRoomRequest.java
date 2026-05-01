d943e8457ac7f5f71cb55c046665e233e96566b4
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-05-01 16:00:14 +0900

    Feat/be#550: 채팅 기능 개선

--- backend/module-chat/src/main/java/com/team6/module/chat/dto/chatRoom/LeaveChatRoomRequest.java
+++
@@ -1,9 +0,0 @@
-package com.team6.module.chat.dto.chatRoom;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LeaveChatRoomRequest(
-        @NotBlank(message = "채팅방 ID는 필수입니다.")
-        String roomId
-) {
-}
